7b9006505313dca3c28c0e1ddcfe9b9115ece411
Alex Aragon <aragcar@gmail.com>
2023-06-20 23:09:44 -0500

    add start

---
+++ start.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . '/config.php';
+$plugin = ProikosPlugin::create();
+$enable = $plugin->get('tool_enable') == 'true';
+$nameTools = $plugin->get_lang('AdManagementSetting');
+
+if ($enable) {
+    if (api_is_platform_admin()) {
+
+        $tpl = new Template($nameTools, true, true, false, false, true, false);
+        $content = $tpl->fetch('proikos/view/proikos_start.tpl');
+        $tpl->assign('content', $content);
+        $tpl->display_one_col_template();
+    }
+}
