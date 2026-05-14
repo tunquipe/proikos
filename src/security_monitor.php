03d7102c9624f48f4b1b8263449bea3fcaba867d
Alex Aragon <aragcar@gmail.com>
2026-05-14 15:06:17 -0500

    fix segurity

--- src/security_monitor.php
+++ src/security_monitor.php
@@ -362,7 +362,7 @@ $dbAdmin   = Database::get_main_table(TABLE_MAIN_ADMIN);
 $sqlOnline = "SELECT teo.login_user_id, teo.user_ip, teo.login_date,
                      u.firstname, u.lastname, u.username, u.status,
                      IF(a.user_id IS NOT NULL, 1, 0) AS is_admin,
-                     TIMESTAMPDIFF(SECOND, teo.login_date, NOW()) AS seconds_ago
+                     UNIX_TIMESTAMP(teo.login_date) AS login_ts
               FROM {$dbOnline} teo
               INNER JOIN {$dbUser} u ON u.id = teo.login_user_id
               LEFT JOIN {$dbAdmin} a ON a.user_id = u.id
@@ -644,7 +644,7 @@ if (empty($onlineUsers)) {
     $i = 1;
     foreach ($onlineUsers as $u) {
         $lastDate = $u['login_date'];
-        $diffSecs = max(0, (int)$u['seconds_ago']);
+        $diffSecs = max(0, time() - (int)$u['login_ts']);
         $mins = floor($diffSecs / 60);
         $secs = $diffSecs % 60;
         if ($diffSecs < 60) {
